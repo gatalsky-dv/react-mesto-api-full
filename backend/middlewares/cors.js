c16467dd7d81de054964d3f69f3a1819ada4608f
unknown <C:\Users\\AppData\Roaming\The Bat!>
2022-11-14 22:57:37 +0500

    bugfix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-  
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
