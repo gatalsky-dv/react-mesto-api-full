d8d4424f1f124f6945436292f2c4c2f6d6271760
unknown <C:\Users\\AppData\Roaming\The Bat!>
2022-11-14 16:43:35 +0500

    edited Accept

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -11,7 +11,7 @@ export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
       method: 'POST',
       headers: {
-          Accept: 'application/json',
+          'Accept': 'application/json',
           'Content-Type': 'application/json'
       },
       body: JSON.stringify({ email, password })
